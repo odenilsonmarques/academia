85b18fa1d163c468db593553693ffa20460d14de
odenilson marques <odmarquesgm@gmail.com>
2020-11-19 00:05:25 -0300

    foi tirado o erro da combobox no formulario de cadastro de pagamento

--- adicionarPagamento.php
+++ adicionarPagamento.php
@@ -1,22 +1,27 @@
+<?php
+include 'config.php';
+?>
 <h1>pagamento</h1>
 
 <form action="recebeActionPagamento.php" method="POST">
-    <label>ALUNO</label>
+
+    <label>ALUNO</label><br/>
     <select name="nome">
         <?php
             $listaAlunos = [];
-            $queryBuscaAluno = $conexaoPDO->prepare("SELECT * FROM alunos");
+            $queryBuscaAluno = $conexaoPDO->query("SELECT * FROM  alunos");
             if($queryBuscaAluno->rowCount() > 0){
-                foreach ($listaAlunos as $alunos){
-        ?>
-            <option value="<?php echo $alunos['id'];?>" ><?php echo $alunos['nome'];?></option>
-        <?php      
+                $listaAlunos = $queryBuscaAluno->fetchAll(PDO::FETCH_ASSOC);
+                foreach($listaAlunos as $alunos){ ?>
+                    <option value="<?=$alunos['nome'];?>"><?=$alunos['nome'];?></option>
+                <?php
+                }
             }
-        }
         ?>
-    </select>
+    </select><br/><br/>
+
     <label>VALOR</label><br/>
     <input type="text" name="valor"><br/><br/>
-   
+
    <input type="submit" value="confirmar pagamento"> 
 </form>
