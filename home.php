2e60e796ba748f2aae59a9398b7795a0b3b8251a
odenilson marques <odmarquesgm@gmail.com>
2020-12-17 21:38:30 -0300

    foi criado o menu da pagina home

--- home.php
+++ home.php
@@ -4,39 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styleHeader.css">
+    <link rel="stylesheet" href="assets/css/styleBody.css">
     <title>Document</title>
 </head>
 <body>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>                        
-            </button>
-            <a class="navbar-brand" href="#myPage">Logo</a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#myPage">ACADEMIA VIGOR</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#about">ABOUT</a></li>
-                <li><a href="#services">SERVICES</a></li>
-                <li><a href="#portfolio">PORTFOLIO</a></li>
-                <li><a href="#pricing">PRICING</a></li>
-                <li><a href="#contact">CONTACT</a></li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#services">SERVIÇOS</a></li>
+                    <li><a href="#portfolio">PORTIFÓLIO</a></li>
+                    <li><a href="#about">SOBRE</a></li>
+                    <li><a href="#contact">LOGIN</a></li>
+                </ul>
             </div>
         </div>
     </nav>
-
-    <div class="jumbotron text-center">
-        <h2>Academia</h2> 
-        <h1>VIGOR</h1>  
-    </div>
-
-   
-    
-
+    <article>
+        <div class="container-fluid titulo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4"></div>
+                    <div class="col-lg-4 text-center infoTitulo">
+                        <h2>Academia</h2> 
+                        <h1>VIGOR</h1>  
+                    </div>
+                    <div class="col-lg-4"></div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <section>
+        <div class="container">
+            <h3>Serviços</h3>
+        </div>
+    </section>
     <script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 </body>
